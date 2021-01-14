c87921dba280b9eb4ce432ffa312d903a1a02b33
Patrick Tuszakowski <mrtall500@gmail.com>
2021-01-14 17:42:07 -0500

    commit tree fd346ad826c98062020b1ae9f7158096b3aae97a

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 class Cat
   # code goes here
+  
+  attr_accessor :mood, :owner
+  attr_reader :name
+  @@all = []
+
+  def initialize(name, owner)
+    @mood = "nervous"
+    @name = name
+    @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
+
+# floffy=Cat.new("floffy", "Mike")
+# binding.pry
